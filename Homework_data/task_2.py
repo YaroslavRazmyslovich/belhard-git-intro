d4c19cb7db8c04a3a031da5209fab3dbfc5f687a
Yaroslav <yaroslav08091989@gmail.com>
2022-02-07 14:46:55 +0300

    gg

--- Homework_data/task_2.py
+++
@@ -1,7 +0,0 @@
-a = float (input ('enter the number а='))
-b = float (input ('enter the number b='))
-c = float (input ('enter the number c='))
-dividend = (a**2 + b**2) / (3*b - 4)
-divider = 4*c**5/6
-print ('the integer part after division is equal to', dividend // divider)
-print ('the remainder after division is equal to', dividend % divider)
